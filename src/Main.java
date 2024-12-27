48f2cc7a4793a277b7ab9641d2a0c13597061052
saramcsantos <contatosarams@gmail.com>
2024-12-27 19:16:00 -0300

    Criação dos objetos produto e serviço, e uso dos métodos: precoTotal e aplicaDesconto

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 import br.com.alura.produto.model.Product;
+import br.com.alura.produto.model.Servico;
 
 public class Main {
     public static void main(String[] args) {
         Product produto = new Product();
-        produto.setNomeDoProduto("Livro Código Limpo");
-        produto.setPrecoDoProduto(72.67);
+        produto.setNome("Caderno");
+        produto.setPrecoProduto(10);
 
-        System.out.println(String.format("Produto: %s Preço orginal: R$ % .2f Preço com desconto: R$ % .2f",
-                produto.getNomeDoProduto(), produto.getPrecoDoProduto(), produto.aplicaDesconto(0.10)));
+        double precoFinalProduto = produto.getPrecoTotalCompra(3);
+
+        System.out.println("Preço total da compra: R$ " + produto.getAplicaDesconto(25, precoFinalProduto));
+
+        Servico servico = new Servico();
+        servico.setPrecoPorHora(34);
+
+        double precoFinalServio = servico.getPrecoTotalCompra(4);
+
+        System.out.println("Preço total do serviço: R$ " + servico.getAplicaDesconto(25, precoFinalServio));
     }
 }
